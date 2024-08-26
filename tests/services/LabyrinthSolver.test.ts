6338316ba4cc38f9a8eb505f799be96239b69677
Joshua <dev.joshuadolor@gmail.com>
2024-08-26 23:52:58 +0200

    unit test for LabyrinthSolver

--- tests/services/LabyrinthSolver.test.ts
+++ tests/services/LabyrinthSolver.test.ts
@@ -2,13 +2,74 @@ import Player, { Movement } from "../../src/entities/Player";
 import Labyrinth, { Position, CellType } from "../../src/entities/Labyrinth";
 import LabyrithSolver from "../../src/services/LabyrinthSolver";
 
-describe("LabyrinthSolver", () => {
+describe("LabyrinthSolver's ", () => {
     let solver: LabyrithSolver;
     let labyrinth: Labyrinth;
 
-    beforeEach(() => {
+    it("getShortestPath should return -1, if Labyrinth given is unsolvable (no start or no exit)", () => {
+        labyrinth = new Labyrinth({ columns: 3, rows: 3 });
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                labyrinth.setCellValue({ row: i, col: j }, CellType.OpenPath);
+            }
+        }
+        solver = new LabyrithSolver(labyrinth);
+        expect(solver.getShortestPath()).toBe(-1);
+    });
+    it("getShortestPath should return -1, if no player can exit the given Labyrinth", () => {
         labyrinth = new Labyrinth({ rows: 5, columns: 5 });
-        const validLabyrinthValue = [
+        const unsolvableLabyrinth = [
+            [
+                CellType.Start,
+                CellType.OpenPath,
+                CellType.Wall,
+                CellType.Wall,
+                CellType.Exit,
+            ],
+            [
+                CellType.Wall,
+                CellType.OpenPath,
+                CellType.Wall,
+                CellType.Wall,
+                CellType.Wall,
+            ],
+            [
+                CellType.Wall,
+                CellType.OpenPath,
+                CellType.OpenPath,
+                CellType.OpenPath,
+                CellType.OpenPath,
+            ],
+            [
+                CellType.OpenPath,
+                CellType.OpenPath,
+                CellType.Wall,
+                CellType.Wall,
+                CellType.Wall,
+            ],
+            [
+                CellType.OpenPath,
+                CellType.OpenPath,
+                CellType.OpenPath,
+                CellType.OpenPath,
+                CellType.OpenPath,
+            ],
+        ];
+        for (let i = 0; i < unsolvableLabyrinth.length; i++) {
+            for (let j = 0; j < unsolvableLabyrinth[i].length; j++) {
+                labyrinth.setCellValue(
+                    { row: i, col: j },
+                    unsolvableLabyrinth[i][j]
+                );
+            }
+        }
+
+        solver = new LabyrithSolver(labyrinth);
+        expect(solver.getShortestPath()).toBe(-1);
+    });
+    it("getShortestPath should return 8, if labyrinth is solvable in 8 moves", () => {
+        labyrinth = new Labyrinth({ rows: 5, columns: 5 });
+        const unsolvableLabyrinth = [
             [
                 CellType.Start,
                 CellType.OpenPath,
@@ -45,20 +106,19 @@ describe("LabyrinthSolver", () => {
                 CellType.OpenPath,
             ],
         ];
-        for (let i = 0; i < validLabyrinthValue.length; i++) {
-            for (let j = 0; j < validLabyrinthValue[i].length; j++) {
+        for (let i = 0; i < unsolvableLabyrinth.length; i++) {
+            for (let j = 0; j < unsolvableLabyrinth[i].length; j++) {
                 labyrinth.setCellValue(
                     { row: i, col: j },
-                    validLabyrinthValue[i][j]
+                    unsolvableLabyrinth[i][j]
                 );
             }
         }
 
         solver = new LabyrithSolver(labyrinth);
+        expect(solver.getShortestPath()).toBe(8);
     });
 
-    it(
-        "ShortestPath should return -1 if Labyrinth given is unsolvable (no start or no exit)"
-    );
-    it("ShortestPath should return -1 if Labyrinth given ");
+    // test private methods??
+    // it("isMoveValid should return true if a move is valid", () => {});
 });
